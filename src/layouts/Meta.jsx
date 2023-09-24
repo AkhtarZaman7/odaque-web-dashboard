4d4aaa410396a22f1a49dbeb621f5196f956504e
AkhtarZaman12 <akhtar.tarar12@gmail.com>
2023-09-24 13:36:27 -0700

    add image

--- src/layouts/Meta.jsx
+++ src/layouts/Meta.jsx
@@ -1,47 +1,47 @@
-"use client";
-import Head from "next/head";
+'use client';
+import Head from 'next/head';
 //import { useRouter } from "next/navigation";
-import { NextSeo } from "next-seo";
+import { NextSeo } from 'next-seo';
 
-import { AppConfig } from "../utils/AppConfig";
+import { AppConfig } from '../utils/AppConfig';
 
 const Meta = (props) => {
   const router = {
-    basePath: "http://localhost:3000",
+    basePath: 'http://localhost:3000',
   };
 
   return (
     <>
       <Head>
-        <meta charSet="UTF-8" key="charset" />
+        <meta charSet='UTF-8' key='charset' />
         <meta
-          name="viewport"
-          content="width=device-width,initial-scale=1"
-          key="viewport"
+          name='viewport'
+          content='width=device-width,initial-scale=1'
+          key='viewport'
         />
         <link
-          rel="apple-touch-icon"
+          rel='apple-touch-icon'
           href={`${router.basePath}/apple-touch-icon.png`}
-          key="apple"
+          key='apple'
         />
         <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
+          rel='icon'
+          type='image/png'
+          sizes='32x32'
           href={`${router.basePath}/favicon-32x32.png`}
-          key="icon32"
+          key='icon32'
         />
         <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
+          rel='icon'
+          type='image/png'
+          sizes='16x16'
           href={`${router.basePath}/favicon-16x16.png`}
-          key="icon16"
+          key='icon16'
         />
         <link
-          rel="icon"
+          rel='icon'
           href={`${router.basePath}/favicon.ico`}
-          key="favicon"
+          key='favicon'
         />
       </Head>
       <NextSeo
@@ -54,6 +54,14 @@ const Meta = (props) => {
           url: props.canonical,
           locale: AppConfig.locale,
           site_name: AppConfig.site_name,
+          images: [
+            {
+              url: `${router.basePath}/assets/images/course.png`,
+              width: 800,
+              height: 600,
+              alt: props.title,
+            },
+          ],
         }}
       />
     </>
